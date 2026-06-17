823062817eeefee918d5e1c0cefd2fdda299476b
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-17 08:36:56 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,66 @@
+console.log("== REUSABLE CALCULATOR FUNCTIONS ==");
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+console.log("Addition:", add(10, 5));
+console.log("Subtraction:", subtract(11, 5));
+console.log("Multiplication:", multiply(12, 5));
+console.log("Division:", divide(10, 5));
+
+console.log("== FIND LARGEST NUMBER IN ARRAY ==");
+
+let numbers = [15, 40, 8, 90, 22];
+
+function findLargest(arr) {
+
+    let largest = arr[0];
+
+    for(let i = 1; i < arr.length; i++) {
+
+        if(arr[i] > largest) {
+            largest = arr[i];
+        }
+
+    }
+
+    return largest;
+}
+
+console.log("Largest Number:", findLargest(numbers));
+
+console.log("== REVERSE ARRAY MANUALLY==");
+
+let fruits = ["Apple", "Orange", "Mango", "Banana"];
+
+function reverseArray(arr) {
+
+    let reversed = [];
+
+    for(let i = arr.length - 1; i >= 0; i--) {
+
+        reversed.push(arr[i]);
+
+    }
+
+    return reversed;
+}
+
+console.log("Original Array:", fruits);
+console.log("Reversed Array:", reverseArray(fruits));
+
+console.log("== REVERSE ARRAY MANUALLY==");
